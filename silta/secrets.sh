a8d382901d8a1ba437157a33dce42482a9b80718
Tormi Tabor <tormi.tabor@gmail.com>
2024-01-03 23:14:15 +0200

    WD-241: Avoid double encryption with lando secrets tool.
    - use the mandatory extension .plain for the decrypted file,
    - do not automatically rename the encryption input file,
    - update usage function with all examples.
    
    Enable vim in `.lando.yml` if you want to use the `lando secrets edit` command.

--- silta/secrets.sh
+++ silta/secrets.sh
@@ -9,8 +9,9 @@ set -eu
 # This function prints the usage message and exits with an error code.
 usage() {
     echo "Usage: $0 {edit|decrypt|encrypt} {target}"
-    echo "Example with secrets.sh: $0 decrypt silta/secrets"
-    echo "Example with Lando: lando secrets decrypt silta/secrets"
+    echo "Edit example: lando secrets edit silta/secrets (uses vim editor)"
+    echo "Decryption example: lando secrets decrypt silta/secrets"
+    echo "Encryption example (note the mandatory .plain extension): lando secrets encrypt silta/secrets.plain"
     exit 1
 }
 
@@ -18,7 +19,6 @@ usage() {
 check_files() {
   if [ ! -f "$1" ]; then
     echo "$1 does not exist."
-    echo "Run 'touch $1' to create a target file first."
     exit 1
   fi
 }
@@ -26,17 +26,17 @@ check_files() {
 # This function decrypts the target file, opens it in vim for editing, and encrypts it again after saving.
 edit_file() {
     check_files "$2" # Check if the target file exists
-    openssl enc -d -aes-256-cbc -pbkdf2 -in "$2" -out "$2.dec" -pass pass:"$ssl_pass" # Decrypt the target file and save it as .dec
-    vim "$2.dec" # Open the decrypted file in vim
-    openssl aes-256-cbc -pbkdf2 -in "$2.dec" -out "$2" -pass pass:"$ssl_pass" # Encrypt the edited file and overwrite the original target file
-    rm "$2.dec" # Remove the decrypted file
+    openssl enc -d -aes-256-cbc -pbkdf2 -in "$2" -out "$2.plain" -pass pass:"$ssl_pass" # Decrypt the target file and save it as .plain
+    vim "$2.plain" # Open the decrypted file in vim
+    openssl aes-256-cbc -pbkdf2 -in "$2.plain" -out "$2" -pass pass:"$ssl_pass" # Encrypt the edited file and overwrite the original target file
+    rm "$2.plain" # Remove the decrypted file
 }
 
-# This function decrypts the target file and saves it as .dec.
+# This function decrypts the target file and saves it as .plain.
 decrypt_file() {
     check_files "$2" # Check if the target file exists
-    openssl enc -d -aes-256-cbc -pbkdf2 -in "$2" -out "$2.dec" -pass pass:"$ssl_pass" # Decrypt the target file and save it as .dec
-    echo "Decrypted into $2.dec" # Print a message
+    openssl enc -d -aes-256-cbc -pbkdf2 -in "$2" -out "$2.plain" -pass pass:"$ssl_pass" # Decrypt the target file and save it as .plain
+    echo "Decrypted into $2.plain" # Print a message
 }
 
 # This function encrypts the target file and overwrites the original file.
@@ -45,12 +45,16 @@ encrypt_file() {
     echo "Encrypting..." # Print a message
     filename=$(basename "$2") # Extract the file name from the path
     extension=${filename##*.} # Extract the file extension from the file name
-    if [ "$extension" != "dec" ]; then # Check if the file extension is not dec
-        mv "$2" "$2.dec" # Save the target file as .dec
+    path="$2" # Store the path in a variable
+    name=${path%.*} # Remove the file extension from the file name
+    if [ "$extension" != "plain" ]; then # Check if the file extension is not .plain
+        echo 'You must use the mandatory .plain extension for the decrypted file.'
+        usage
+        exit 1
     fi
-    openssl aes-256-cbc -pbkdf2 -in "$2.dec" -out "$2" -pass pass:"$ssl_pass" # Encrypt the target file and overwrite the original file
-    rm "$2.dec" # Remove the decrypted file
-    echo "Encrypted into $2" # Print a message
+    openssl aes-256-cbc -pbkdf2 -in "$2" -out "$name" -pass pass:"$ssl_pass" # Encrypt the target file and overwrite the original file
+    rm "$2" # Remove the decrypted file
+    echo "Encrypted into $name" # Print a message
 }
 
 # This block checks the number of arguments and the first argument, and calls the appropriate function
