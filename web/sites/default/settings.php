8806f07ea39728d0f81c594bb814d2051b7f7fea
Tormi Tabor <tormi.tabor@gmail.com>
2024-04-23 10:18:10 +0300

    WD-245: Use the DRUPAL_ROOT constant consistently

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -28,18 +28,9 @@ $settings['file_public_path']  = 'sites/default/files';
 $settings['config_sync_directory'] = '../config/sync';
 
 // Load services definition file.
-$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/services.yml';
 
-/**
- * The default list of directories that will be ignored by Drupal's file API.
- *
- * By default ignore node_modules and bower_components folders to avoid issues
- * with common frontend tools and recursive scanning of directories looking for
- * extensions.
- *
- * @see file_scan_directory()
- * @see \Drupal\Core\Extension\ExtensionDiscovery::scanDirectory()
- */
+// The default list of directories that will be ignored by Drupal's file API.
 $settings['file_scan_ignore_directories'] = [
   'node_modules',
   'bower_components',
@@ -88,19 +79,12 @@ switch ($env) {
     break;
 }
 
-/**
- * Load local development override configuration, if available.
- *
- * Use settings.local.php to override variables on secondary (staging,
- * development, etc) installations of this site. Typically used to disable
- * caching, JavaScript/CSS compression, re-routing of outgoing emails, and
- * other things that should not happen on development and testing sites.
- */
-if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-  include $app_root . '/' . $site_path . '/settings.local.php';
+// Local environment configuration overrides.
+if (file_exists(DRUPAL_ROOT . '/sites/default/settings.local.php')) {
+  include DRUPAL_ROOT . '/sites/default/settings.local.php';
 }
 
 // Silta cluster configuration overrides.
-if (getenv('SILTA_CLUSTER') && file_exists($app_root . '/' . $site_path . '/settings.silta.php')) {
-  include $app_root . '/' . $site_path . '/settings.silta.php';
+if (getenv('SILTA_CLUSTER') && file_exists(DRUPAL_ROOT . '/sites/default/settings.silta.php')) {
+  include DRUPAL_ROOT . '/sites/default/settings.silta.php';
 }
