18f7554e43775c1423e7612035dff236f6e437db
Tormi Tabor <tormi.tabor@gmail.com>
2024-04-24 09:13:39 +0300

    WD-245: Streamline mounting usage in Adminer setup

--- .lando/adminer.sh
+++ .lando/adminer.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -exu
 
-# Link custom adminer.php from .lando folder to webroot.
-ln -sf /app/.lando/adminer.php /var/www/html/
-
 # Copy & rename versioned Adminer library file.
 cd /var/www/html
 find . -type f -name 'adminer-*' -print0 | xargs -0 -I{} cp -f {} adminer_library.php
