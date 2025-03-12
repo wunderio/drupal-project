42d92c1a8e560cd409c7d97efc598a7c018d7984
Tormi Tabor <tormi.tabor@gmail.com>
2025-03-13 01:29:19 +0200

    QAG-31: fix(varnish) Update Varnish purger config to use correct port

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -44,7 +44,7 @@ $settings['file_scan_ignore_directories'] = [
 // Varnish Purge configuration.
 if (getenv('VARNISH_ADMIN_HOST')) {
   $config['varnish_purger.settings.c6013ef2d0']['hostname'] = trim(getenv('VARNISH_ADMIN_HOST'));
-  $config['varnish_purger.settings.c6013ef2d0']['port'] = getenv('VARNISH_ADMIN_PORT') ? trim(getenv('VARNISH_ADMIN_PORT')) : '80';
+  $config['varnish_purger.settings.c6013ef2d0']['port'] = '80';
 }
 
 // Symfony Mailer configuration.
