be051bfe8c23ad36a8252ff21dec8f97e8abd7df
Joonas Heinonen <joonas.heinonen@wunder.io>
2026-01-28 15:38:00 +0200

    xdebug-n-lando-removal: Converted adminer.sh and drupal.sh into ddev commands

--- .lando/drupal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Description: run the Drupal core script with provided arguments
-## Usage: drupal <arguments>
-## Example: lando drupal generate-theme my-theme
-
-cd ${LANDO_WEBROOT}
-
-php core/scripts/drupal "$@"
