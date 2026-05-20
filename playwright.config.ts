646116c42b8d3d5abb2738335aa927ba5e9549f3
Marc Galang <marc.galang@wunder.io>
2026-05-20 14:15:04 +0300

    WQS-115: Add screenshot configuration for test failures in Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /* Take screenshot on test failure. See https://playwright.dev/docs/screenshots */
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
